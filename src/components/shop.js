c2f2afb99b130db2f7f931097444812c420bc470
Isabirye1515 <isabiryeelijah15@gmail.com>
2024-12-04 15:14:01 +0300

    shop

--- src/components/shop.js
+++ src/components/shop.js
@@ -1,17 +1,17 @@
-import { List } from '@carbon/icons-react';
-import { Column, Grid, Search, Select, SelectItem } from '@carbon/react';
+import { Calendar, ChartLine, List, ShoppingBag } from '@carbon/icons-react';
+import { Column, Grid, Row, Search, Select, SelectItem, Tag } from '@carbon/react';
 import React from 'react';
 
 const Shop = () => {
     return (
-        <div>
-            <Grid>
-                <Column lg={3}  >
+        <div className='m-4' >
+            <Grid className='m-2' >
+                <Column lg={3} md={2} sm={4} >
                 <div>
                     <h2>Price Range</h2>
                 </div>
                 </Column >
-                <Column lg={6}  >
+                <Column lg={6} md={4} sm={4}  >
                 <div>
                     <Search
                     id='search'
@@ -21,7 +21,7 @@ const Shop = () => {
                 </div>
                 </Column>
                 
-                <Column lg={5} >
+                <Column lg={5} md={3} sm={2} >
                 <Select>
                     <SelectItem   
                     text="Featured" />
@@ -35,16 +35,31 @@ const Shop = () => {
                 </Select>
 
                 </Column>
-                <Column  lg={2} >
+                <Column  lg={2} md={2} sm={2} >
                 <List size={30} />
                 </Column>
 
             </Grid>
-            <Grid>
-                <Column>
+
+
+            
+            <Grid  className='m-2' >
+                <Column lg={4}md={2} sm={4} >
                 <div>
-                    
+                <input type="range" min="0" max="100" value="50" step="1" />
                 </div>
+                </Column>
+                <Column lg={12} md={6} sm={4} >
+                <div>
+                <Calendar size={100} />
+                </div>
+                
+                <div>
+
+                    <h2>Booking Fees</h2>
+                    <Tag>Published</Tag>
+                </div>
+        
                 </Column>
             </Grid>
         </div>
