dc1771f3682d230b04f218c7eec5ebced0934bfb
Isabirye1515 <isabiryeelijah15@gmail.com>
2024-12-04 15:48:22 +0300

    contact

--- src/components/contactus.js
+++ src/components/contactus.js
@@ -1,9 +1,70 @@
+import { Email, EmailNew, MessageQueue, Phone } from '@carbon/icons-react';
+import { Column, Form, Grid, TextArea, TextInput } from '@carbon/react';
 import React from 'react';
 
 const Contactus = () => {
     return (
         <div>
-            
+            <Grid className='m-4' >
+
+                <Column  lg={16} md={8} sm={4} >
+                <div className=' bg-white p-2' >
+                    <h1>Contact Us</h1>
+                </div>
+
+                <h2>Contact us about anything related to our company or services.
+                We'll do our best to get back to you as soon as possible.</h2>
+                </Column>
+                <Column lg={10} md={5} sm={4}  >
+
+                <Form>
+                    <TextInput
+                    id='name'
+                    labelText="name"
+                    placeholder='Name'
+                     />
+                     <TextInput
+                    id='email'
+                    labelText="email"
+                    placeholder='Email'
+                    type='email'
+                     />
+                     <TextInput
+                    id='number'
+                    labelText="number"
+                    placeholder='Phone Number'
+                    type='tel'
+                     />
+                     <TextInput
+                    id='company'
+                    labelText="company"
+                    placeholder='Company'
+                    type='text'
+                     />
+                     <TextInput
+                    id='subject'
+                    labelText="sbject"
+                    placeholder='subject'
+                    type='text'
+
+                     />
+                     <TextArea
+                    id='text'
+                    labelText="text"
+                    placeholder='Question'
+                    
+                     />
+                </Form>
+                </Column>
+                <Column lg={4} md={3} sm={4} >
+                <div className='text-info' >
+                    <h2 className='text-white'>Connect with us</h2>
+                  <MessageQueue size={40}   />  Contact us <br/>
+                    <Email size={40} /> info@yourcompany.example.com<br/>
+                    <Phone size={40} />  +1 555-555-5556<br/>
+                </div  >
+                </Column>
+            </Grid>
         </div>
     );
 }
