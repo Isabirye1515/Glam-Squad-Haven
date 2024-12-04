9f155005f3440fe9a89ba8182b18aef576153d19
Isabirye1515 <isabiryeelijah15@gmail.com>
2024-12-04 14:33:14 +0300

    hey to

--- src/components/company/news.js
+++ src/components/company/news.js
@@ -1,9 +1,45 @@
+import { Column, Grid, Search, Tag } from '@carbon/react';
 import React from 'react';
 
 const News = () => {
     return (
         <div>
-            
+            <Grid>
+                <Column  lg={16} md={8} sm={4}  >
+                <div> 
+                    <h1 className='h-1 bg-red' >News</h1>
+                </div>
+                </Column>
+                <Column lg={8} md={4} sm={4} >
+                <div>
+                    <b>Blogs :</b>
+                <Tag>All</Tag>
+                    <Tag>news</Tag>
+                    <Tag>Blogs</Tag>
+                    <Tag>Success Stories</Tag>
+                </div>
+                </Column>
+                <Column lg={8} md={4} sm={4} >
+
+                <div>
+                    <Search
+                    id='search'
+                    labelText="Search"
+                    aria-label='Search'
+                     />
+                </div>
+                </Column>
+                <Column lg={16} md={8} sm={4} >
+                <div className='container rounded bg-white m-2' >
+                    <h1>
+                        There is no News yet !
+                    </h1>
+
+                </div>
+                </Column>
+
+
+            </Grid>
         </div>
     );
 }
--- src/components/shop.js
+++ src/components/shop.js
@@ -1,9 +1,52 @@
+import { List } from '@carbon/icons-react';
+import { Column, Grid, Search, Select, SelectItem } from '@carbon/react';
 import React from 'react';
 
 const Shop = () => {
     return (
         <div>
-            
+            <Grid>
+                <Column lg={3}  >
+                <div>
+                    <h2>Price Range</h2>
+                </div>
+                </Column >
+                <Column lg={6}  >
+                <div>
+                    <Search
+                    id='search'
+                    labelText="Search"
+                    aria-label='Search'
+                     />
+                </div>
+                </Column>
+                
+                <Column lg={5} >
+                <Select>
+                    <SelectItem   
+                    text="Featured" />
+                    <SelectItem
+                    text="Newest Arrival" />
+
+                    <SelectItem text="Name(A-Z)" />
+                    <SelectItem text="Price -Low to High"/>
+                    <SelectItem  text="Price -High to Low" />
+
+                </Select>
+
+                </Column>
+                <Column  lg={2} >
+                <List size={30} />
+                </Column>
+
+            </Grid>
+            <Grid>
+                <Column>
+                <div>
+                    
+                </div>
+                </Column>
+            </Grid>
         </div>
     );
 }
