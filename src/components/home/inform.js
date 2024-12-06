2010f871c90cc871760527cd76be90d5b8cd4f90
Isabirye1515 <isabiryeelijah15@gmail.com>
2024-12-06 12:10:13 +0300

    hey to

--- src/components/home/inform.js
+++ src/components/home/inform.js
@@ -43,10 +43,10 @@ const Inform = () => {
         <div className='org'  >
             <center>
             
-            <Slider {...settings} className='slider shadow p-5' >
+            <Slider {...settings} className='slider' >
                 {slide.map((item) => (
-                    <div key={item.id} className='slide p-2' >
-                        <h2 className='fw-lighter' >{item.infor}</h2>
+                    <div key={item.id} className='slide' >
+                        <h2  >{item.infor}</h2>
                         <img
                             src={item.image}
                             height={40}
