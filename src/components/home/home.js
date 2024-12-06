7d31dbe89de7dd478cc031df0bc9f1e80f69849d
Isabirye1515 <isabiryeelijah15@gmail.com>
2024-12-06 16:33:01 +0300

    jooojo

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -11,19 +11,6 @@ const Home = () => {
         <div >
             <div   className='home-up p-5 '>
         
-            {/* Navigation Buttons */}
-           
-        
-            {/* Welcome Section */}
-            <Grid className='hello  '>
-                <Column  lg={16} md={8} sm={4}  className='m-1 ' >
-                <h1 className='m-3' > The Glam squad haven </h1>
-                <p>
-                To become the ultimate beauty and wellness destination, renowned for our exceptional services, talented team, and commitment to empowering our clients to live their best lives
-                </p>
-                </Column>
-                
-            </Grid>
             </div>
             <div   className='hero'><HeroSecton /></div>
             <div><Tell /></div>
